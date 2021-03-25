dd3c220cabaf59bb6ee4f70e29917df3a7135ceb
kamitoshi <kroo.aq.74@gmail.com>
2021-03-25 14:58:03 +0900

    env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,7 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
@@ -6,7 +10,8 @@ unless Rails.env.development? || Rails.env.test?
       aws_secret_access_key: ENV['S3_SECRET_KEY'],
       region: ENV['S3_REGION']
     }
-
+    config.asset_host = 'https://s3.amazonaws.com/pickup-images'
+    config.fog_public = true
     config.fog_directory  = 'pickup-images'
     config.cache_storage = :fog
   end
