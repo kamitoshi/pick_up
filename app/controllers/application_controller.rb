f391d9c2c9ede58f15c21b203f84ed513113f75c
kamitoshi <kroo.aq.74@gmail.com>
2021-04-09 10:06:14 +0900

    modify home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,14 @@ class ApplicationController < ActionController::Base
   def admin_or_shop!
     unless admin_signed_in? || shop_signed_in?
       flash[:danger] = "店舗ユーザーとしてログインしてください"
-      redirect_to root_path
+      redirect_to new_shop_session_path
     end
   end
 
   def admin_or_user!
     unless admin_signed_in? || user_signed_in?
       flash[:danger] = "ログインしてください"
-      redirect_to root_path
+      redirect_to new_user_session_path
     end
   end
 
